6aa1fb2e3753dd2e6ba59d884fc83f6260b9c75f
Yahya Yasir Koca <yykoca.08@gmail.com>
2021-09-07 10:54:39 +0200

    model Link created

---
+++ Day 2/backend/models/Link.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const LinkSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    beschreibung: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    }
+})
+
+const Link = mongoose.model('Link', LinkSchema);
+
+async function register (title, beschreibung, url) {
+	const link = new Link({
+		title,
+		beschreibung,
+        link: url,
+	});
+
+	return await link.save();
+}
+
+async function readOne(id) {
+    return await Link.findOne({_id: id});
+}
+
+async function readAll() {
+    return await Link.find();
+}
+
+async function updateOne(title, beschreibung, link) {
+    return await Link.deleteOne({_id: id});
+}
+
+async function deleteOne(id) {
+    return await Link.deleteOne({_id: id});
+}
+
+
+module.exports = {
+	register, readOne, readAll, updateOne, deleteOne
+};
